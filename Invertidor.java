754066b46277b3abdd516e434d55553e19ab4a2c
Daniel Tapia <dtapias@unsa.edu.pe>
2022-05-13 18:55:15 -0500

    Se preparo los prints de la clase

---
+++ Invertidor.java
@@ -0,0 +1,17 @@
+import javafx.scene.paint.Stop;
+
+class Invertidor{
+    public static void main(String[] args) {
+        int[] array = {0,1,2,3,4};
+        System.out.println("Inicio de invertidor de arrays");
+        System.out.println("Array ingresado: ");
+        for (int i : array) {
+            System.out.print(array[i]);
+        }
+        System.out.println("\nArray invertido: ");
+    }
+    public int[] invertirArray(int[] a){
+        
+        return null;
+    }
+}
