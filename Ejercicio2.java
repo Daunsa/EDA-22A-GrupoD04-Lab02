8c5bf34c687fa3d2484d2d0d03c9730e75550c58
benjaminsuclle <msuclles@unsa.edu.pe>
2022-05-13 23:11:33 -0500

    Me di cuenta de un error en implementación

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
+
+import javafx.print.PrintColor;
 public class Ejercicio2 {
-	private int numeroatrasladar = 2;
+	private static int numeroatrasladar = 3;
 	public static void main(String[] args){
 		int[] arreglooriginal = {1,2,3,4,5};
+		imprimirArreglo(arreglooriginal);
 		int[] arreglotrasladado = rotarIzquierdaArray(arreglooriginal);
-
-
-
-		
+		imprimirArreglo(arreglotrasladado);
 	}
 
 	public static int[] rotarIzquierdaArray(int[] A) {
@@ -17,17 +17,21 @@ public class Ejercicio2 {
 		int longituddeA = A.length;
 		for(int i=0; i<longituddeA; i++){
 			int j = i+numeroatrasladar;
-			if(j>longituddeA)
+			if(j>longituddeA-1)
 				j=j%longituddeA;
-			tempovar = A [i];
+			//System.out.println(i+" intercambia con "+j);
+			int tempovar = A [i];
 			A [i] = A[j];
 			A [j] = tempovar;
-
-
-			
-			
 		}
 		return A;
+	}
+	public static void imprimirArreglo(int [] arreglo){
+		System.out.print("El arreglo es: ");
+		for(int i=0;i<arreglo.length;i++){
+			System.out.print(arreglo[i]+" ");
+		}
+		System.out.print("\n");
 	}
 
 }
