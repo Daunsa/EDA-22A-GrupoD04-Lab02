2071a08f3931c14ec37314c680f904187e8031ea
Daniel Tapia <dtapias@unsa.edu.pe>
2022-05-13 19:13:24 -0500

    Se finalizo el ejercicio

--- Invertidor.java
+++ Invertidor.java
@@ -5,13 +5,21 @@ class Invertidor{
         int[] array = {0,1,2,3,4};
         System.out.println("Inicio de invertidor de arrays");
         System.out.println("Array ingresado: ");
-        for (int i : array) {
+        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
         }
+        array = invertirArray(array);
         System.out.println("\nArray invertido: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
     }
-    public int[] invertirArray(int[] a){
-        
-        return null;
+    public static int[] invertirArray(int[] a){
+        int length = a.length;
+        int[] array = new int[length];
+        for (int i = 0; i < length;i++) {
+            array[length-1-i] = a[i];
+        }
+        return array;
     }
 }
