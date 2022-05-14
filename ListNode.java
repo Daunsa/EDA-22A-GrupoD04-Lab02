8830926bc1c3e3a3a5bb19988f6de327e056b2cd
benjaminsuclle <msuclles@unsa.edu.pe>
2022-05-14 00:39:37 -0500

    Metodo add terminado

--- ListNode.java
+++ ListNode.java
@@ -4,4 +4,27 @@ public class ListNode<T> {
         root = new Node<T>(value);
     }
     //Metodos de clase listNode
+
+    void add(int i, T value){
+        if(i < 0 || i > (this.largo - 1)){
+            return;
+        }
+        if (i==0){
+            Node temp = this.root;
+            this.root = new Node(value);
+            this.root.nextNode = temp;
+        }
+        Node temp = this.root;
+        for (int j = 0; j < this.largo; j++) {
+            if(j == (i-1)){
+                temp = this.root.nextNode;
+                this.root.nextNode=new Node(value);
+                this.root.nextNode.nextNode=temp;
+                break;
+            }else{
+                temp = this.root.nextNode;
+            }
+        }
+    }
+
 }
