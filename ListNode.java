0d6d1015d15792c9e76a366547c4a3d1bd9e949d
RonaldGarciaValdivia <rgarciava@unsa.edu.pe>
2022-05-13 22:37:03 -0500

    Se añadio metodo Size

--- ListNode.java
+++ ListNode.java
@@ -1,7 +1,11 @@
 public class ListNode<T> {
     Node<T> root;
+    private int largo = 0;
     public ListNode(T value){
         root = new Node<T>(value);
     }
     //Metodos de clase listNode
+    public int size(){
+        return largo;
+    }
 }
