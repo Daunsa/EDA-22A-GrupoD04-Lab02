b64b3d154738e0171905788f3353f01c4121b21f
benjaminsuclle <msuclles@unsa.edu.pe>
2022-05-13 22:53:05 -0500

    Primer intento

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 public class Ejercicio2 {
-	private numeroatrasladar = 2;
+	private int numeroatrasladar = 2;
 	public static void main(String[] args){
 		int[] arreglooriginal = {1,2,3,4,5};
 		int[] arreglotrasladado = rotarIzquierdaArray(arreglooriginal);
@@ -14,13 +14,20 @@ public class Ejercicio2 {
 		/** */
 		// Procedimiento para rotar la matriz
 		/** */
-		for(int i=0; i<A.length; i++){
+		int longituddeA = A.length;
+		for(int i=0; i<longituddeA; i++){
+			int j = i+numeroatrasladar;
+			if(j>longituddeA)
+				j=j%longituddeA;
+			tempovar = A [i];
+			A [i] = A[j];
+			A [j] = tempovar;
+
 
 			
 			
 		}
-		int [] borrar  = {1,2};
-		return borrar;
+		return A;
 	}
 
 }
