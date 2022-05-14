c29aa2e2a09dddcc2a0c9d73b640b1cd3f9595b9
benjaminsuclle <msuclles@unsa.edu.pe>
2022-05-13 22:44:21 -0500

    Hice un metodo prueba

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,7 +1,23 @@
 import java.util.ArrayList;
 public class Ejercicio2 {
 	public static void main(String[] args){
+		int[] arreglooriginal = {1,2,3,4,5};
+		int[] arreglotrasladado = rotarIzquierdaArray(arreglooriginal);
 
+
+
+		
+	}
+
+	public static int[] rotarIzquierdaArray(int[] A) {
+		/** */
+		// Procedimiento para rotar la matriz
+		/** */
+		for(int i=0; i<A.length; i++){
+			
+		}
+		int [] borrar  = {1,2};
+		return borrar;
 	}
 
 }
