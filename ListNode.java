f2e05550a22bc139b7041ac03212a1088784b4f3
Daniel Tapia <dtapias@unsa.edu.pe>
2022-05-13 23:00:16 -0500

    agragada la funcion equals

--- ListNode.java
+++ ListNode.java
@@ -34,4 +34,18 @@ public class ListNode<T> {
         }
         remove(root);
     }
+
+    public boolean equals(ListNode list){
+        return equals(list.root);
+    }
+    public boolean equals(Node root1, Node root2){
+        if(root1.nextNode != null && root2.nextNode != null){
+            if(root1.value != root2.value){
+                return false;
+            }else{
+                return equals(root1.nextNode, root2.nextNode);
+            }
+        }
+        return true;
+    }
 }
