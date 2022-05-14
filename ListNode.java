b1ae1b08aa769e2e8747973fcfbe04c650de2d96
Daniel Tapia <dtapias@unsa.edu.pe>
2022-05-13 21:53:05 -0500

    metodo addAll creada, este solo funciona si el metodo add existe

--- ListNode.java
+++ ListNode.java
@@ -4,4 +4,13 @@ public class ListNode<T> {
         root = new Node<T>(value);
     }
     //Metodos de clase listNode
+    public boolean addAll(T[] c){
+        if(c.length == 0)
+            return false;
+        for (int i = 0; i < c.length; i++) {
+            add(c[i]);
+        }
+        return true;
+    }
+
 }
