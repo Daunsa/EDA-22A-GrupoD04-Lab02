bb4a5ce2ff3f45dc3942011342ef5a685c3f7d35
benjaminsuclle <msuclles@unsa.edu.pe>
2022-05-15 02:29:13 -0500

    NOte que vscode añadio espacios nuevos inncesarios

--- ListNode.java
+++ ListNode.java
@@ -2,166 +2,152 @@ import java.util.ListIterator;
 
 public class ListNode<T> {
     Node<T> root;
-    private final int largo;
-
-    public ListNode(final T  value){
+    private int largo = 0;
+    public ListNode(T value){
         root = new Node<T>(value);
     }
-
-    // Metodos de clase listNode
-    public boolean addAll(final  T[] c){
-        if (c.length == 0)
+    //Metodos de clase listNode
+    public boolean addAll(T[] c){
+        if(c.length == 0)
             return false;
         for (int i = 0; i < c.length; i++) {
             add(c[i]);
         }
         return true;
     }
-
-    public boolean removeAll(final Nod e[] c){
-        if (c.length == 0)
+    
+    public boolean removeAll(Node[] c){
+        if(c.length == 0)
             return false;
         for (int i = 0; i < c.length; i++) {
             remove(c[i]);
         }
         return true;
     }
-
-    public void clear() {
-        if (root != null) {
+    
+    public void clear(){
+        if(root != null){
             clear(this.root);
         }
     }
 
-    private void clear(final Node  root){
-        if (root.nextNode != null) {
+    private void clear(Node root){
+        if(root.nextNode != null){
             clear(root.nextNode);
         }
         remove(root);
     }
 
-    public boolean equals(final ListNode  list){
+    public boolean equals(ListNode list){
         return equals(list.root);
     }
-
-    public boolean equals(final Node root1, final  Node root2){
-        if (root1.nextNode != null && root2.nextNode != null) {
-            if (root1.value != root2.value) {
+    public boolean equals(Node root1, Node root2){
+        if(root1.nextNode != null && root2.nextNode != null){
+            if(root1.value != root2.value){
                 return false;
-            } else {
+            }else{
                 return equals(root1.nextNode, root2.nextNode);
             }
         }
         return true;
     }
-
-    public Node get(final  int i){
-        if (i < 0 || i > (largo - 1)) {
+    public Node get(int i){
+        if(i < 0 || i > (largo - 1)){
             return null;
         }
         Node temp = this.root;
         for (int j = 0; j < largo; j++) {
-            if (j == i) {
+            if(j == i){
                 return temp;
-            } else {
+            }else{
                 temp = root.nextNode;
             }
         }
         return null;
     }
-
-    public boolean set(final int i, fina l Node nodo){
-        if (i < 0 || i > (largo - 1)) {
+    public boolean set(int i, Node nodo){
+        if(i < 0 || i > (largo - 1)){
             System.out.println("Fuera de rango");
             return false;
         }
-        if (i == 0) {
-            final Node temp = root.nextNode;
+        if(i == 0){
+            Node temp = root.nextNode;
             root = nodo;
             root.nextNode = temp;
         }
         Node temp = this.root;
         for (int j = 0; j < largo; j++) {
-            if (j == (i - 1)) {
+            if(j == (i - 1)){
                 temp.nextNode = nodo;
                 break;
-            } else {
+            }else{
                 temp = root.nextNode;
             }
         }
         return true;
     }
-
-    public int size() {
+    public int size(){
         return largo;
     }
-
-    public boolean isEmpty() {
-        if (this.root != null) {
+    public boolean isEmpty(){
+        if(this.root != null){
             return false;
         }
         return true;
     }
-
-    public boolean contains(final T  valor){
+    public boolean contains(T valor){
         return contains(valor, this.root);
     }
-
-    private boolean contains(final T valor, fina l Node root){
-        if (root.value == valor) {
+    private boolean contains (T valor, Node root){
+        if(root.value == valor){
             return true;
         }
-        if (root.nextNode != null) {
+        if(root.nextNode != null){
             contains(valor, root.nextNode);
         }
         return false;
     }
-
-    public boolean add(final T  valor){
-        if (this.root == null) {
+    
+    public boolean add(T valor){
+        if(this.root == null){
             this.root = new Node(valor);
             return true;
         }
         return add(valor, this.root);
     }
-
-    private boolean add(final T valor, final N ode<T> root){
-        if (root.nextNode != null) {
+    private boolean add(T valor, Node root){
+        if(root.nextNode != null){
             add(valor, root.nextNode);
         }
-        this.root.nextNode = new Node<T>(valor);
-        return true;
+        root.nextNode = new Node(valor);
+        return true;   
     }
-
-    public boolean remove(final T  valor){
+    public boolean remove(T valor){
         return remove(valor, this.root)[0];
     }
-
-    private Object[] remove (final T valor, Node<T> root){
+    private Object[] remove (T valor, Node root){
         if(root.nextNode != null){
-            final Object[] arraytemp = remove(valor, root.nextNode);
+            Object[] arraytemp = remove(valor, root.nextNode);
             if(arraytemp[0]){
                 root.nextNode = arraytemp[1];
             }
             return {false, null};
         }
         if(root.value == valor){
-		    final Node<T> temp = this.root.nextNode;
+		    Node temp = root.nextNode;
             root = null;
             return {true,temp};
         }
         return {false, null};
     }
-
-    public boolean containsAll(fnal T  [] c){
-        for (int i = 0; i < c.length; i++) {
-            if (!contains(c[i])) {
+    public boolean containsAll(T [] c){
+        for(int i=0; i<c.length; i++){
+            if(!contains(c[i])){
                 return false;
             }
         }
         return true;
     }
-
     public void add(final int i, T value){
         if (i < 0 || i > (this.largo - 1)) {
             return;
